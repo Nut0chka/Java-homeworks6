940b5c57a491d2e1ad9e6e0127afa6972612b390
Anna Demidova <annad.va00@gmail.com>
2024-01-07 17:51:29 +0300

    Initial commit

---
+++ src/main/java/ru/netology/stats/StatsService.java
@@ -0,0 +1,69 @@
+package ru.netology.stats;
+
+public class StatsService {
+
+    public int sumSales(int[] sales) { //сумма продаж
+        int sum = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            sum = sum + sales[i];
+        }
+
+        return sum;
+    }
+
+    public int AverageSales(int[] sales) { //средняя сумма продаж
+        int sum = sumSales(sales);
+        int average = sum / sales.length;
+
+        return average;
+
+    }
+
+    public int maxSumMonth(int[] sales) { // номер месяца с максимальными продажами
+        int maxMonth = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] >= sales[maxMonth]) {
+                maxMonth = i;
+            }
+        }
+        return maxMonth + 1;
+    }
+
+    public int minSumMonth(int[] sales) { // номер месяца с минимальными продажами
+        int minMonth = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] <= sales[minMonth]) {
+                minMonth = i;
+            }
+        }
+        return minMonth + 1;
+    }
+
+    public int AboveAverageSales(int[] sales) { // количество месяцев с продажами выше среднего
+        int average = AverageSales(sales);
+        int count = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] > average) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public int BelowAverageSales(int[] sales) { // количество месяцев с продажами ниже среднего
+        int average = AverageSales(sales);
+        int count = 0;
+
+        for (int i = 0; i < sales.length; i++) {
+            if (sales[i] < average) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
